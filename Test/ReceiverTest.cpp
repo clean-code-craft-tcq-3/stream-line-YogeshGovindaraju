8f6e57cd82ee57f8a2642b1f6f5b02bc0366b407
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-08-12 01:44:26 +0530

    Delete ReceiverTest.cpp

--- Test/ReceiverTest.cpp
+++
@@ -1,9 +0,0 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-
-#include "test/catch.hpp"
-#include "Receiver.c"
-
-TEST_CASE("call reeciever")
-{
-  void ReceiveSensorData();
-}
