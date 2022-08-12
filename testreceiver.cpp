77471e455977360748c545d9085e9dd25acb254a
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-08-12 08:19:10 +0530

    Update testreceiver.cpp

--- testreceiver.cpp
+++ testreceiver.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
-#include "Receiver.c"
+#include "Receiver.h"
 
 TEST_CASE("call receiver"){
 void ReceiveSensorData();
