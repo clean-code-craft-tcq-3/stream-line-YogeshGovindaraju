7b752a9f3709359d8daebcac0c8d7a99365125b7
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-08-13 22:37:42 +0530

    Update testreceiver.cpp

--- Receiver/testreceiver.cpp
+++ Receiver/testreceiver.cpp
@@ -4,17 +4,18 @@
 #include "Receiver.h"
 
 
-// TEST_CASE("test case 1 : check min value functionality "){
-//   int minValue = 0,
-//   int sensorData[50] ={0};
-  
-//   sensorData[2] = -1;
-//   sensorData[42] = -3;
-//   GetMaxValue(&minValue,&sensorData);
-  
-//   REQUIRE(minValue == -3);
-// }
+
 
 TEST_CASE("call receiver"){
   ReceiveSensorData();
+}
+TEST_CASE("test case 1 : check min value functionality "){
+  int minValue = 0,
+  int sensorData[50] ={0};
+  
+  sensorData[2] = -1;
+  sensorData[42] = -3;
+  GetMaxValue(&minValue,&sensorData);
+  
+  REQUIRE(minValue == -3);
 }
